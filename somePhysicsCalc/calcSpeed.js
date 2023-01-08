c14ca676b5402f8b3dd3a3269ea69c9c332e31b0
abel12-tech <myfirstgit9@gmail.com>
2023-01-08 23:04:51 +0300

    Calculation of some formula

---
+++ somePhysicsCalc/calcSpeed.js
@@ -0,0 +1,10 @@
+const allForm = document.getElementById('velocity-form')
+const checker = document.getElementById('checker')
+
+allForm.addEventListener('submit',submitAllInput)
+
+function submitAllInput(e){
+    const initialDisplacement = document.getElementById('initial-displacement')
+
+    e.preventDefault()
+}
