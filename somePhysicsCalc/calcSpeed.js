c7fb1af1382e819706cf8e802c5f106c6ce2b34c
abel12-tech <myfirstgit9@gmail.com>
2023-01-09 23:24:25 +0300

    some solved calculation is added

--- somePhysicsCalc/calcSpeed.js
+++ somePhysicsCalc/calcSpeed.js
@@ -4,7 +4,27 @@ const checker = document.getElementById('checker')
 allForm.addEventListener('submit',submitAllInput)
 
 function submitAllInput(e){
-    const initialDisplacement = document.getElementById('initial-displacement')
-
     e.preventDefault()
+    //   Input
+    const initialDisplacement = document.getElementById('initial-displacement').value
+    const finalDisplacement = document.getElementById('final-displacement').value
+    const initialTime = document.getElementById('initial-time').value
+    const finalTime = document.getElementById('final-time').value
+    const mass = document.getElementById('mass').value
+    
+    
+
+    const changeInDisp = parseFloat(finalDisplacement)-parseFloat(initialDisplacement)
+    const changeInTime = parseFloat(finalTime)-parseFloat(initialTime)
+    const velocity = changeInDisp / changeInTime
+    document.getElementById('acceleration').value =velocity
+
+
+    // Output
+ 
+
+
+    
+
+  
 }
